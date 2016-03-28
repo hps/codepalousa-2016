17cd359d46b9bd8c3a08867329b9ddf6b60b3406
Shane Logsdon <shane.logsdon@e-hps.com>
2016-03-28 00:14:11 -0400

    initial commit

---
+++ CodePaLousa/Controllers/PaymentController.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+using SecureSubmit.Entities;
+using SecureSubmit.Infrastructure;
+using SecureSubmit.Services;
+
+using CodePaLousa.ViewModels;
+using SecureSubmit.Fluent.Services;
+using System.Configuration;
+
+namespace CodePaLousa.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult Index()
+        {
+            return View(new PaymentDetails
+            {
+                PublicKey = ConfigurationManager.AppSettings["PublicKey"],
+                Amount = 1.00m
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Process(PaymentDetails details)
+        {
+            var config = new HpsServicesConfig
+            {
+                SecretApiKey = ConfigurationManager.AppSettings["SecretKey"]
+            };
+            var service = new HpsFluentCreditService(config);
+            HpsAuthorization response = null;
+
+            try
+            {
+                response = service.Authorize()
+                    .WithAmount(details.Amount)
+                    .WithToken(details.PaymentToken)
+                    .WithCardHolder(new HpsCardHolder
+                    {
+                        Address = new HpsAddress { Zip = details.BillingZip }
+                    })
+                    .Execute();
+            }
+            catch (HpsException e)
+            {
+
+            }
+            return View(response);
+        }
+    }
+}
---
+++ CodePaLousa/ViewModels/PaymentDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodePaLousa.ViewModels
+{
+    public class PaymentDetails
+    {
+        public string PublicKey { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentToken { get; set; }
+        public string BillingZip { get; set; }
+    }
+}
